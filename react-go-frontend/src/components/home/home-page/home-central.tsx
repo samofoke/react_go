3680d4ead3f76a4423626331332893395fb69fee
samofoke <sabataernestmofokeng@gmail.com>
2024-08-25 20:54:58 +0200

    adding mock data

--- react-go-frontend/src/components/home/home-page/home-central.tsx
+++ react-go-frontend/src/components/home/home-page/home-central.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import RaffleTicket from "../../../assets/iamges/raffle-tickets.jpg";
+import { Link } from "react-router-dom";
 
 const HomeCentral: React.FC = () => {
   return (
@@ -7,12 +8,14 @@ const HomeCentral: React.FC = () => {
       <div className="text-center">
         <h2>Find a movie to watch.</h2>
         <hr />
-        <img
-          src={RaffleTicket}
-          alt="movie ticket"
-          className="img-fluid d-block"
-          style={{ maxWidth: "200px", margin: "auto" }}
-        />
+        <Link to="/movies">
+          <img
+            src={RaffleTicket}
+            alt="movie ticket"
+            className="img-fluid d-block"
+            style={{ maxWidth: "200px", margin: "auto" }}
+          />
+        </Link>
       </div>
     </>
   );
--- react-go-frontend/src/components/movies/movies.tsx
+++ react-go-frontend/src/components/movies/movies.tsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const MoviesSection: React.FC = () => {
+  const [movies, setMovies] = useState([]);
+
+  const movieList = [
+    {
+      id: 1,
+      title: "God of War",
+      release_date: "2001-04-05",
+      runtime: 120,
+      mpaa_rating: "PG-13",
+      description: "this is always long",
+    },
+    {
+      id: 2,
+      title: "Sword Art Online",
+      release_date: "2015-04-05",
+      runtime: 120,
+      mpaa_rating: "PG-13",
+      description: "don't worry too much",
+    },
+  ];
+
+  console.log("learning types: ", movieList);
+
+  useEffect(() => {
+    setMovies(movieList);
+  }, []);
+
   return (
     <>
       <div className="text-center">
