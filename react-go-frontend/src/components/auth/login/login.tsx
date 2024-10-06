963e070547d7a6369dd260be61cb5511e4cd9bef
samofoke <sabataernestmofokeng@gmail.com>
2024-10-06 10:39:44 +0200

    adding the login form

--- react-go-frontend/src/components/auth/login/login.tsx
+++ react-go-frontend/src/components/auth/login/login.tsx
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
-
+import InputField from "../../custom-components/input";
 
 interface EmailAnPassword {
   email: string;
   password: string;
 }
 
-const Login: React.FC<EmailAnPassword> = ({email, password}) => {
-  const [userEmail, setUserEmail] = useState<string>("")
+const Login: React.FC<EmailAnPassword> = ({ email, password }) => {
+  const [userEmail, setUserEmail] = useState<string>("");
 
-  console.log("email and password: ", email, password)
-  return <h2>The Login page</h2>;
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  };
+
+  console.log("email and password: ", email, password);
+  return (
+    <div className="col-md-6 offset-md-3">
+      <h2>The Login page</h2>
+      <hr />
+
+      <form onSubmit={handleSubmit}>
+        <InputField
+          title="Email Address"
+          type="email"
+          className="form-control"
+          name="email"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
